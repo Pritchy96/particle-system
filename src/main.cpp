b8f835ebca435f4f8fedd18008d01af355bbcb6a
Tom Pritchard <tompritchard1996@gmail.com>
2018-09-27 14:35:46 +0100

    Gentle comment cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,19 @@ vector<vec3> axis_colours = {
 	vec3(0.0f, 0.0f, 1.0f)
 };
 
+vector<vec3> test_data_lines = {
+	vec3(0.000000, 4.000000, 12.000000),
+	vec3(2.870316, -101.411970, 1.000000),
+
+	vec3(0.000000, 0.000000, 0.2233231),
+	vec3(3.897837, 14.548908, 1.000000),
+
+	vec3(4.398729, 18.116240, 2.213213),
+	vec3(5.055743, 6.247947, 1.000000),
+
+	vec3(5.365756, 6.035206, 4.000000),
+	vec3(5.693254, 14.517317, 1.00000)
+};
 
 int main(int argc, const char* argv[]) {
     cout << "Launching Program" << endl;
@@ -42,6 +55,8 @@ int main(int argc, const char* argv[]) {
 	GLuint basicShader = LoadShaders("./bin/shaders/basic.vertshader", "./bin/shaders/basic.fragshader");
 
     renderer->addRenderable(*new Renderable(basicShader, axis_lines, axis_colours));
+    renderer->addRenderable(*new Renderable(basicShader, test_data_lines));
+
 
     while (true) {  //TODO: Write proper update&exit logic.
         renderer->update();
